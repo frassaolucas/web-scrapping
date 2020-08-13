7d7d45e8a6f3799aa8484b0ed3f3417be1116ba8
Lucas Frassao <frassao.lucas@gmail.com>
2020-08-13 17:04:07 -0300

    practicing consuming api

---
+++ index.js
@@ -0,0 +1,32 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(`https://instagram.com/mileneinoue`);
+
+  const imgList = await page.evaluate(() => {
+    // get images inside posts area
+    const nodeList = document.querySelectorAll('article img');
+
+    // transform NodeList to Array
+    const imgArray = [...nodeList];
+
+    // transform nodes to JS objects
+    const imgList = imgArray.map(({ src }) => ({
+      src,
+    }));
+
+    return imgList;
+  });
+
+  // save data to local file (json)
+  fs.writeFile('instagram.json', JSON.stringify(imgList, null, 2), err => {
+    if (err) throw new Error('something went wrong');
+
+    console.log('well done');
+  });
+
+  await browser.close();
+})();
